68d10322ec35264d861cac9d30a50afeac7c1d80
“inziani” <ValentineRobai.Inziani@gmail.com>
2020-08-09 00:14:12 +0300

    re-createthe reporitory

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// let theNumber = Number(prompt("Key in a number"))
+// if (!Number.isNaN(theNumber)){
+//   confirm("Your number is the square root of " + " "+ theNumber*theNumber)
+// }
+// else{
+//   confirm("Why didn't you give me the number?")
+// };
+
+// 1;
+// !false;
+
+// let number = 0;
+// let myList = [];
+// while(number<=100){
+//   number+=2;
+//   myList.push(number);
+// };
+// alert(myList);
+
+let yourName;
+do{yourName=prompt("Who are you");
+}
+while(!yourName);
+alert(yourName);
+  
