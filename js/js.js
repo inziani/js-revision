0c3c9c5446f0fc8b8886dabbdd01582b51134333
“inziani” <ValentineRobai.Inziani@gmail.com>
2020-08-11 16:48:39 +0300

    link font awesome, google fonts and js script files to the index page

---
+++ js/js.js
@@ -0,0 +1,52 @@
+// alert('hello people');
+// switch (prompt('what is the weather today?')){
+//   case 'rainy':
+//     alert('bring an umbrella');
+//   case 'sunny':
+//     alert('dress lightly');
+//     break;
+//   case 'cloudy':
+//     alert('go outside');
+//     break;
+//   default:
+//       alert('unknown weather type');
+//     break;
+// }
+
+// let result = ' ';
+// let counter = 0;
+// while (result.length<8){
+//   result = result + '#';
+//   counter+=1
+//   alert(result)
+// }
+
+// for(var line ='#'; line.length<8; line+='#')
+// alert(line)
+
+// var myList = [];
+// for (num=0; num <=100; num+=1){
+//   if (num%3===0){
+//     myList.push('Fizz');
+//   }
+//   else if (num%5===0){
+//     myList.push('buzz');
+//   }
+//   else {
+//     myList.push(num);
+//   }
+// // alert(myList)
+// }
+
+// myList
+var myList = [];
+for(num=10; num<=0; num-=1){
+  myList.push(num)
+}
+
+// function countDown(num){
+//   alert(num)
+//   if (num===0) return;
+//   alert(num);
+// }
+// countDown(10)
